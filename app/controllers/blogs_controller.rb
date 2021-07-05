8029fdf94c76272a0cb62e1d56e381590cf1c585
Yuuki <ukmmm0523@gmail.com>
2021-07-05 18:31:27 +0900

    変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,25 +1,36 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
-    binding.pry
   end
   def new
     @blog = Blog.new
   end
   def create
-    Blog.create(blog_params)
+   @blog = Blog.new(blog_params)
     if @blog.save
     redirect_to new_blog_path, notice: "ブログを作成しました！"
     else
       render :new
     end
   end
-  def edit
   
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
+  
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  
   def show
     @blog = Blog.find(params[:id])
   end
+ 
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
